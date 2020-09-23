18e9722bbe6df5af0c1f6354aa9c8d3a74925597
quantunm <xiang_ye@outlook.com>
2020-09-23 23:25:13 +0800

    new file:   log/000001.SZ.csv 	modified:   log/000300.SH.csv 	modified:   test.py

--- test.py
+++ test.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
 
     rsrs = RSRS.CRSRS_std()
     #rsrs = RSRS.CRSRS_slope()
-    #rsrs.run_test(df_benchmark, 20000, 0.0003, 0.001, 0.01, 0.7, -0.7, 
-    #    '20100104', '20171204',df_FCR, df_BCR, df_NCR, 18, 600)
+    rsrs.run_test(df_benchmark, 20000, 0.0003, 0.001, 0.0, 0.7, -0.7, 
+        '20100104', '20200914',df_FCR, df_BCR, df_NCR, 18, 600)
     
     #rsrs.run_test(df_benchmark, 20000, 0.0, 0.0, 0.0, 0.7, -0.7, 
     #    '20150105', '20200914',df_FCR, df_BCR, df_NCR, 18, 600)
 
-    rsrs.run_test(df_benchmark, 2000000, 0.0003, 0.001, 0.01, 0.7, -0.7, 
-        '20100104', '20171204',df_benchmark, df_benchmark, df_benchmark, 18, 600)
+    #rsrs.run_test(df_benchmark, 2000000, 0.0003, 0.001, 0.0, 0.7, -0.7, 
+    #    '20100104', '20200914',df_benchmark, df_benchmark, df_benchmark, 18, 600)
 
     #rsrs.run_test(df_benchmark, 2000000, 0.0, 0.0, 0.0, 0.7, -0.7, 
     #    '20150105', '20200914',df_benchmark, df_benchmark, df_benchmark, 18, 600)
